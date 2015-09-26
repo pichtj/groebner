b32594ba23bec78ce98fc2a0c4979981035c7957
Jonathan Picht <jonathan.picht@jpicht.de>
2015-09-26 18:11:32 +0200

    Added libmpir.

--- Makefile
+++ Makefile
@@ -1,15 +1,30 @@
-CFLAGS := -m64 -L/opt/local/lib
-CPPFLAGS := -std=c++14
-CC := $(shell which gcc-mp-4.9 || echo gcc) $(CFLAGS)
-CPP := $(shell which g++-mp-4.9 || echo g++) $(CFLAGS) $(CPPFLAGS)
+BUILDDIR := $(shell pwd)
+CFLAGS := -m64 -I$(BUILDDIR)/include -L$(BUILDDIR)/lib -std=c++14
+CPPFLAGS :=
+CC := $(shell which gcc-mp-4.9 || echo gcc)
+CXX := $(shell which g++-mp-4.9 || echo g++)
 
 all: moGVW
 
-moGVW: main.o
-	$(CPP) -o $@ $^ -lgmp
+moGVW: main.o lib/libmpirxx.a lib/libmpir.a
+	$(CXX) $(CFLAGS) -o $@ $^ -lmpirxx -lmpir
 
-main.o: main.cpp Polynomial.h LabelledMonomial.h
-	$(CPP) -c -o $@ $<
+main.o: main.cpp Polynomial.h LabelledMonomial.h include/mpir.h include/mpirxx.h
+	$(CXX) $(CFLAGS) $(CPPFLAGS) -c -o $@ $<
 
 clean:
 	rm -rf moGVW *.o
+
+distclean: clean
+	rm -rf include lib share mpir-2.7.0*
+
+mpir-2.7.0.tar.bz2:
+	wget http://mpir.org/mpir-2.7.0.tar.bz2
+
+mpir-2.7.0: mpir-2.7.0.tar.bz2
+	tar jxvf $<
+
+lib/libmpir.a include/mpir.h include/mpirxx.h: mpir-2.7.0
+	cd $< && CC="$(CC)" ./configure --enable-cxx --disable-shared --enable-static --prefix=$(BUILDDIR) && make && make install
+
+libs: lib/libmpir.a
--- main.cpp
+++ main.cpp
@@ -1,27 +1,53 @@
+#include <mpirxx.h>
+
 #include <cstdlib>
 #include "LabelledMonomial.h"
 using namespace std;
 
+void print(int i) {
+  printf("%i", i);
+}
+
 template<class E>
-void printExponent(const Exponent<E>& exp) {
-  printf("^{");
-  for (int i = 0; i < VAR_COUNT; ++i) printf("%i ", exp[i]);
-  printf("}\n");
+void print(const Exponent<E>& exp) {
+  printf("{");
+  for (int i = 0; i < VAR_COUNT; ++i) {
+    print(exp[i]);
+    if (i < VAR_COUNT - 1) printf(" ");
+  }
+  printf("}");
 }
 
 template<class C, class E>
-void printPolynomial(const Polynomial<C, E>& p) {
+void print(const Term<C, E>& t) {
+  print(t.coeff);
+  printf("*x^");
+  print(t.exp);
+}
+
+template<class C, class E>
+void print(const Polynomial<C, E>& p) {
   
 }
 
 int main(int argc, char* argv[]) {
-  Exponent<short> exp;
+  mpir_ui a;
+  Exponent<char> exp;
   exp[23] = 17;
   auto exp2 = exp * exp;
-  printExponent(exp2/exp/exp);
-  printf("sizeof(Exponent<char>) = %i\n", sizeof(Exponent<char>));
-  printf("sizeof(Exponent<short>) = %i\n", sizeof(Exponent<short>));
-  printf("sizeof(Exponent<int>) = %i\n", sizeof(Exponent<int>));
-  printf("sizeof(Exponent<long>) = %i\n", sizeof(Exponent<long>));
+  print(exp2/exp/exp);
+  printf("\n");
+  printf("sizeof(mpz_t) = %i\n", sizeof(mpz_t));
+  printf("sizeof(mpz_class) = %i\n", sizeof(mpz_class));
+  printf("sizeof(Polynomial<mpz_class, char>) = %i\n", sizeof(Polynomial<mpz_class, char>));
+  printf("sizeof(Polynomial<mpz_class, short>) = %i\n", sizeof(Polynomial<mpz_class, short>));
+  printf("sizeof(Polynomial<mpz_class, int>) = %i\n", sizeof(Polynomial<mpz_class, int>));
+  printf("sizeof(Polynomial<mpz_class, long>) = %i\n", sizeof(Polynomial<mpz_class, long>));
+
+  Term<int,char> t;
+  t.coeff = 23;
+  t.exp = exp;
+  print(t);
+  printf("\n");
   return 0;
 }
