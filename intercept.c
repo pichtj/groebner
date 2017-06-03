a4a83fbd8c95bf0c723bb1197b6c116cfe6f41a9
Jonathan Picht <jonathan.picht@jpicht.de>
2017-06-03 07:42:10 +0000

    Added memory usage logging

--- intercept.c
+++ intercept.c
@@ -4,7 +4,18 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <time.h>
- 
+
+void log_memory_status() {
+    FILE *f = fopen("/proc/self/statm", "r");
+    if (!f) return;
+    unsigned long size,resident,share,text,lib,data,dt;
+    if (7 != fscanf(f,"%ld %ld %ld %ld %ld %ld %ld", &size, &resident, &share, &text, &lib, &data, &dt)) return;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    fprintf(stderr, "\n%ld.%06ld %ld %ld %ld %ld %ld %ld %ld", ts.tv_sec, ts.tv_nsec, size, resident, share, text, lib, data, dt);
+    fclose(f);
+}
+
 static int (*real_fflush)(FILE*) = NULL;
 
 void load_real_fflush() {
@@ -20,6 +31,7 @@ int fflush(FILE* stream) {
         load_real_fflush();
     }
     if (stream == stderr) {
+        log_memory_status();
         struct timespec ts;
         clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
         fprintf(stream, "\n%ld.%06ld ", ts.tv_sec, ts.tv_nsec);
